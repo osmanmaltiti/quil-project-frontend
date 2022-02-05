0226d8efd05e236029a697ce3c4577f5fd70b279
osmanmaltiti <terabyte7778@gmail.com>
2022-02-05 10:45:48 -1200

    version 1.1

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -3,14 +3,13 @@ import { getAuth } from "firebase/auth";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_API_KEY,
-  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_SENDER_ID,
-  appId: process.env.REACT_APP_APP_ID
+  apiKey: "AIzaSyDcilGQk54W5QBe31Frs9Z9fkLm5B7rtK8",
+  authDomain: "project-one-2c857.firebaseapp.com",
+  projectId: "project-one-2c857",
+  storageBucket: "project-one-2c857.appspot.com",
+  messagingSenderId: "879701329151",
+  appId: "1:879701329151:web:bfe085e521ce2edb3d38ca"
 };
-
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
